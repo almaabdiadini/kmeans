8f436ed7cbc1e8948e58a4c7d75cf1f1970ab2a8
almaabdiadini <alma.abdiadini92@gmail.com>
2019-08-23 17:08:58 +0700

    update terbaru

--- application/views/admin/sb_admin/page/v_dashboard.php
+++ application/views/admin/sb_admin/page/v_dashboard.php
@@ -1,2 +1,19 @@
 <center><img src="<?php echo base_url("assets/admin/login/logo.jpeg")  ?>"></center>
 
+
+<script src="js/jquery.js"></script
+
+
+    <aside id="fh5co-hero" class="js-fullheight">
+    <div class="flexslider js-fullheight">
+    <ul class="slides">
+
+    <div class="overlay-gradient"></div>
+    <div class="container">
+    <div class="col-md-6 col-md-offset-3 col-md-pull-3 js-fullheight slider-text">
+    <div class="slider-text-inner">
+    <div class="desc">
+        <h2>Selamat Datang Admin</h2>
+        <b><p>Website ini digunakan untuk menghitung dan mengetahui loyalitas pelanggan 
+</p></b>
+
